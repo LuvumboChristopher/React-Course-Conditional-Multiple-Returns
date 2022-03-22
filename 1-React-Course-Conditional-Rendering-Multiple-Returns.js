adef95bd573003356fb44e725bbb4773fe956899
Christopher Luvumbo <christopherluvumbo.dev@gmail.com>
2022-03-22 17:13:16 +0100

    Add files via upload

---
+++ 1-React-Course-Conditional-Rendering-Multiple-Returns.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+const url = 'https://api.github.com/users/QuincyLarson';
+
+const MultipleReturns = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('Default user')
+
+
+  useEffect(()=>{
+    fetch(url)
+    .then((res) => {
+      if(res.status >= 200 && res.status <= 299){
+        return res.json();
+      } else {
+        setIsLoading(false)
+        setIsError(true)
+        throw new Error(res.statusText)
+      }  
+    })
+    .then((user)=> {
+      const {login} = user;
+      setUser(login)
+      setIsLoading(false)
+    })
+
+    .catch((err)=> console.error(err))
+  }, [])
+
+  if(isLoading){
+    return <h1>Loading...</h1>
+  }
+
+  if (isError) {
+    return <h1>Error...</h1>
+  }
+
+
+  return (
+    <>
+      <h1>{user}</h1>
+    </>
+  );
+};
+
+export default MultipleReturns;
